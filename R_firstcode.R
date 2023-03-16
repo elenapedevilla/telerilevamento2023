91c4b5904b0b5a7c05043a8b798576999eb9cad0
elenapedevilla <127427386+elenapedevilla@users.noreply.github.com>
2023-03-16 17:44:45 +0100

    Update R_firstcode.R

--- R_firstcode.R
+++ R_firstcode.R
@@ -7,16 +7,51 @@ library(raster)
 setwd("C:/lab/")
 
 #import
-l2011<-brick("p224r63__2011_masked.grd")
+l2011<-brick("p224r63_2011_masked.grd")
 l2011
 
 #plot
 plot(l2011)
 
-cl<-colorRampPalette(c(“red”, “orange”, “yellow”)) (100)
+cl<-colorRampPalette(c("red", "orange", "yellow"))(100)
 plot(l2011, col=cl)
 
 plot(l2011[[4]], col=cl) or plot(l2011$B4_sre, col=cl)
 
 nir<-l2011[[4]] or: nir<-l2011$B4_sre
 plot(nir, col=cl)
+
+#Exercise: change the colour gamut for all the images (another range of colours, not red, orange, yellow)
+
+#Exercise: plot the NIR band (only the 4th one, like last time)
+
+#dev.off() closes graphs
+
+#Export graphs in R
+pdf("myfirstgraph.pdf")
+plot(l2011$B4_sre, col=cl)
+dev.off()
+
+#Plotting two bands in a multiframe
+par(mfrow=c(2,1)) 
+plot(l2011[[3]], col=cl)
+plot(l2011[[4]], col=cl)
+
+#Plotting the first four bands/layers in a multiframe
+par(mfrow=c(2,2))
+
+#blue
+clb<-colorRampPalette(c("blue4", "blue2", "light blue"))(100)
+plot(l2011[[1]], col=clb)
+
+#green
+clg<-colorRampPalette(c("chartreuse4", "chartreuse3", "chartreuse"))(100)
+plot(l2011[[2]], col=clg)
+
+#red
+clr<-colorRampPalette(c("brown", "brown1", "darkred"))(100)
+plot(l2011[[3]], col=clr)
+
+#infrared
+cln<-colorRampPalette(c("darkorcihd", "darkorchid3", "darkorchid4"))(100)
+plot(l2011[[4]], col=cln)
