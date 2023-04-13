6d20b56d9a7616a0386885d92208c212a7ccabc3
elenapedevilla <127427386+elenapedevilla@users.noreply.github.com>
2023-04-13 17:47:34 +0200

    Create R_code_classification.R

---
+++ R_code_classification.R
@@ -0,0 +1,9 @@
+# Classification of remote sensing data
+
+library(raster)
+
+setwd("C:/lab/")
+
+so <- brick("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
+
+plotRGB(so, 1, 2, 3, stretch="lin")
