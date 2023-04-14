21edad8751b59f2f07d4b88320f0db66ff93b10b
elenapedevilla <127427386+elenapedevilla@users.noreply.github.com>
2023-04-14 17:34:13 +0200

    Update R_code_classification.R

--- R_code_classification.R
+++ R_code_classification.R
@@ -7,3 +7,77 @@ setwd("C:/lab/")
 so <- brick("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
 
 plotRGB(so, 1, 2, 3, stretch="lin")
+plotRGB(so, 1, 2, 3, stretch="hist")
+
+# Classifying the solar data
+
+# https://rspatial.org/raster/rs/4-unsupclassification.html
+
+# 1. Get all the single values
+singlenr <- getValues(so)
+singlenr
+
+# 2. Classify
+kcluster <- kmeans(singlenr, centers = 3)
+kcluster
+
+# 3. Set values to a raster on the basis of so
+soclass <- setValues(so[[1]], kcluster$cluster) # assign new values to a raster object
+
+cl <- colorRampPalette(c('yellow','black','red'))(100)
+plot(soclass, col=cl)
+
+#class 3: highest energy level
+#class 1: lowest energy level
+#class 2: medium energy level
+
+frequencies <- freq(soclass)
+frequencies
+tot = 2221440
+percentages = frequencies * 100 /  tot
+
+#class 3: highest energy level: 21.2%
+#class 1: lowest energy level: 41.4%
+#class 2: medium energy level: 37.3%
+
+
+# set.seed can be used for repeating the experiment in the same manner for N times
+# http://rfunction.com/archives/62
+
+
+
+# day 2 Grand Canyon
+
+gc <- brick("dolansprings_oli_2013088_canyon_lrg.jpg")
+gc
+
+# rosso = 1
+# verde = 2
+# blu = 3
+
+plotRGB(gc, r=1, g=2, b=3, stretch="lin")
+
+# change the stretch to histogram stretching
+plotRGB(gc, r=1, g=2, b=3, stretch="hist")
+
+# classification
+
+# 1. Get values
+singlenr <- getValues(gc)
+singlenr
+
+# 2. Classify
+kcluster <- kmeans(singlenr, centers = 3)
+kcluster
+
+# 3. Set values
+gcclass <- setValues(gc[[1]], kcluster$cluster) # assign new values to a raster object
+
+cl <- colorRampPalette(c('yellow','black','red'))(100)
+plot(gcclass, col=cl)
+
+frequencies <- freq(gcclass)
+tot = 58076148
+percentages = frequencies * 100 /  tot
+
+# Exercise: classify the map with 4 classes
