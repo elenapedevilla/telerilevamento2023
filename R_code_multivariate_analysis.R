e326d9c5386c6f5c97b18f0e7d469f0b8e95603a
elenapedevilla <127427386+elenapedevilla@users.noreply.github.com>
2023-04-28 17:10:42 +0200

    Create R_code_multivariate_analysis.R

---
+++ R_code_multivariate_analysis.R
@@ -0,0 +1,47 @@
+# How to reduce multidimensional dataset
+
+library(raster)
+library(ggplot2)
+library(viridis)
+setwd("C:/lab/") 
+
+sen <- brick("sentinel.png")
+
+sen
+plot(sen)
+
+sen2 <- stack(sen[[1]], sen[[2]], sen[[3]])
+
+plot(sen2)
+
+pairs(sen2)
+
+# PCA (Principal Component Analysis)
+sample <- sampleRandom(sen2, 10000)
+sample
+pca <- prcomp(sample)
+
+# variance explained
+summary(pca)
+
+# correlation with original bands
+pca
+
+# pc map
+pci <- predict(sen2, pca, index=c(1:2))
+
+plot(pci[[1]])
+
+# ggplot
+pcid <- as.data.frame(pci[[1]], xy=T)
+
+ggplot() +
+geom_raster(pcid, mapping = aes(x=x, y=y, fill=PC1)) +
+scale_fill_viridis()
+
+
+# speeding up analyses
+# aggregate cells: resampling (ricampionamento)
+senres <- aggregate(sen, fact=10)
+
+# then repeat the PCA analysis
