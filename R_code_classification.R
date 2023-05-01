fd5c87e0260f47aff41a13bf13f92fbe008713da
elenapedevilla <127427386+elenapedevilla@users.noreply.github.com>
2023-05-01 18:52:01 +0200

    Update R_code_classification.R

--- R_code_classification.R
+++ R_code_classification.R
@@ -27,18 +27,18 @@ soclass <- setValues(so[[1]], kcluster$cluster) # assign new values to a raster 
 cl <- colorRampPalette(c('yellow','black','red'))(100)
 plot(soclass, col=cl)
 
-#class 3: highest energy level
-#class 1: lowest energy level
-#class 2: medium energy level
+# Class 3: highest energy level
+# Class 1: lowest energy level
+# Class 2: medium energy level
 
 frequencies <- freq(soclass)
 frequencies
 tot = 2221440
 percentages = frequencies * 100 /  tot
 
-#class 3: highest energy level: 21.2%
-#class 1: lowest energy level: 41.4%
-#class 2: medium energy level: 37.3%
+# Class 3: highest energy level: 21.2%
+# Class 1: lowest energy level: 41.4%
+# Class 2: medium energy level: 37.3%
 
 
 # set.seed can be used for repeating the experiment in the same manner for N times
@@ -55,22 +55,22 @@ setwd("C:/lab/")
 gc <- brick("dolansprings_oli_2013088_canyon_lrg.jpg")
 gc
 
-# rosso = 1
-# verde = 2
-# blu = 3
+# Red = 1
+# Green = 2
+# Blue = 3
 
 plotRGB(gc, r=1, g=2, b=3, stretch="lin")
 
-# change the stretch to histogram stretching
+# Change the stretch to histogram stretching
 plotRGB(gc, r=1, g=2, b=3, stretch="hist")
 
-#The image is quite big; let's crop it
+# The image is quite big; let's crop it
 gcc <- crop(gc, drawExtent())
 gcc 
 plotRGB(gcc, r=1, g=2, b=3, stretch="lin")
 ncell(gcc)
 
-# classification
+# Classification
 
 # 1. Get values
 singlenr <- getValues(gcc)
@@ -81,15 +81,15 @@ kcluster <- kmeans(singlenr, centers = 3)
 kcluster
 
 # 3. Set values
-gccclass <- setValues(gcc[[1]], kcluster$cluster) # assign new values to a raster object
+gccclass <- setValues(gcc[[1]], kcluster$cluster) # Assign new values to a raster object
 gccclass
 
 cl <- colorRampPalette(c('yellow','black','red'))(100)
 plot(gccclass, col=cl)
 
-#class 1: conglomerates
-#class 2: volcanic rocks
-#class 3: sandstone
+# Class 1: conglomerates
+# Class 2: volcanic rocks
+# Class 3: sandstone
 
 frequencies <- freq(gccclass)
 frequencies
